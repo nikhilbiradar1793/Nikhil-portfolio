0b28f675a0899dff6dec0c26e068dfaddbba1bed
Nikhil Biradar <nikhilbiradar1793@gmail.com>
2026-06-01 05:57:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function showMessage() {
+
+document.getElementById("msg").innerHTML =
+"Thank you for visiting my portfolio!";
+}
+
+const uploadPhoto =
+document.getElementById("uploadPhoto");
+
+const profilePic =
+document.querySelector(".profile-pic");
+
+uploadPhoto.addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(file){
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+profilePic.src = e.target.result;
+
+};
+
+reader.readAsDataURL(file);
+
+}
+
+});
